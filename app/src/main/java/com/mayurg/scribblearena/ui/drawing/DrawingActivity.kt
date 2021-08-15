869490e7f9e87f6c9054f50a4c2675f1012de8f0
Mayur <mbg51195@gmail.com>
2021-08-15 10:19:59 +0530

    - Setup for receiving DrawData from server done

--- app/src/main/java/com/mayurg/scribblearena/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/mayurg/scribblearena/ui/drawing/DrawingActivity.kt
@@ -3,6 +3,7 @@ package com.mayurg.scribblearena.ui.drawing
 import android.graphics.Color
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -16,6 +17,7 @@ import androidx.navigation.navArgs
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import com.mayurg.scribblearena.R
+import com.mayurg.scribblearena.data.remote.ws.models.DrawAction
 import com.mayurg.scribblearena.data.remote.ws.models.GameError
 import com.mayurg.scribblearena.data.remote.ws.models.JoinRoomHandshake
 import com.mayurg.scribblearena.databinding.ActivityDrawingBinding
@@ -55,6 +57,8 @@ class DrawingActivity : AppCompatActivity() {
         toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
         toggle.syncState()
 
+        binding.drawingView.roomName = args.roomName
+
         val header = layoutInflater.inflate(R.layout.nav_drawer_header, binding.navView)
         rvPlayers = header.findViewById(R.id.rvPlayers)
         binding.root.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
@@ -76,6 +80,13 @@ class DrawingActivity : AppCompatActivity() {
             override fun onDrawerStateChanged(newState: Int) = Unit
 
         })
+
+        binding.ibUndo.setOnClickListener {
+            if (binding.drawingView.isUserDrawing) {
+                binding.drawingView.undo()
+                viewModel.sendBaseModel(DrawAction(DrawAction.ACTION_UNDO))
+            }
+        }
 
         binding.colorGroup.setOnCheckedChangeListener { group, checkedId ->
             viewModel.checkRadioButton(checkedId)
@@ -135,6 +146,27 @@ class DrawingActivity : AppCompatActivity() {
     private fun listenToSocketEvents() = lifecycleScope.launchWhenStarted {
         viewModel.socketEvent.collect { event ->
             when (event) {
+                is DrawingViewModel.SocketEvent.DrawDataEvent -> {
+                    val drawData = event.data
+                    if (!binding.drawingView.isUserDrawing) {
+                        when (drawData.motionEvent) {
+                            MotionEvent.ACTION_DOWN -> {
+                                binding.drawingView.startedTouchExternally(drawData)
+                            }
+                            MotionEvent.ACTION_MOVE -> {
+                                binding.drawingView.movedTouchExternally(drawData)
+                            }
+                            MotionEvent.ACTION_UP -> {
+                                binding.drawingView.releasedTouchExternally(drawData)
+                            }
+                        }
+                    }
+                }
+
+                is DrawingViewModel.SocketEvent.UndoEvent -> {
+                    binding.drawingView.undo()
+                }
+
                 is DrawingViewModel.SocketEvent.GameErrorEvent -> {
                     when (event.data.errorType) {
                         GameError.ERROR_ROOM_NOT_FOUND -> finish()
--- app/src/main/java/com/mayurg/scribblearena/ui/views/DrawingView.kt
+++ app/src/main/java/com/mayurg/scribblearena/ui/views/DrawingView.kt
@@ -43,6 +43,7 @@ class DrawingView @JvmOverloads constructor(
     private var paths = Stack<PathData>()
     private var pathDataChangedListener: ((Stack<PathData>) -> Unit)? = null
 
+    private var startedTouch = false
     var roomName: String? = null
 
     var isUserDrawing = false
@@ -85,6 +86,61 @@ class DrawingView @JvmOverloads constructor(
             strokeWidth = initialThickness
         }
         canvas?.drawPath(path, paint)
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        path.reset()
+        invalidate()
+    }
+
+    fun undo() {
+        if (paths.isNotEmpty()) {
+            paths.pop()
+            pathDataChangedListener?.let { change ->
+                change(paths)
+            }
+            invalidate()
+        }
+    }
+
+    fun startedTouchExternally(drawData: DrawData) {
+        parseDrawData(drawData).apply {
+            paint.color = color
+            paint.strokeWidth = thickness
+            path.reset()
+            path.moveTo(fromX, fromY)
+            invalidate()
+            startedTouch = true
+        }
+    }
+
+    fun movedTouchExternally(drawData: DrawData) {
+        parseDrawData(drawData).apply {
+            val dx = abs(toX - fromX)
+            val dy = abs(toY - fromY)
+            if (!startedTouch) {
+                startedTouchExternally(drawData)
+            }
+            if (dx >= smoothness || dy >= smoothness) {
+                path.quadTo(fromX, fromY, (fromX + toX) / 2f, (fromY + toY) / 2f)
+                invalidate()
+            }
+        }
+    }
+
+    fun releasedTouchExternally(drawData: DrawData) {
+        parseDrawData(drawData).apply {
+            path.lineTo(fromX, fromY)
+            canvas?.drawPath(path, paint)
+            paths.push(PathData(path, paint.color, paint.strokeWidth))
+            pathDataChangedListener?.let { change ->
+                change(paths)
+            }
+            path = Path()
+            invalidate()
+            startedTouch = false
+        }
     }
 
     private fun startedTouch(x: Float, y: Float) {
@@ -144,6 +200,15 @@ class DrawingView @JvmOverloads constructor(
             ACTION_UP -> releasedTouch()
         }
         return true
+    }
+
+    private fun parseDrawData(drawData: DrawData): DrawData {
+        return drawData.copy(
+            fromX = drawData.fromX * viewWidth!!,
+            fromY = drawData.fromY * viewHeight!!,
+            toX = drawData.toX * viewWidth!!,
+            toY = drawData.toY * viewHeight!!
+        )
     }
 
     private fun createDrawData(
